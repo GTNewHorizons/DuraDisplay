dc0757ca959b94480d6ef7fb7b026f1e1e205140
ghostflyby <ghostflyby+git@outlook.com>
2023-08-04 23:38:44 +0800

    Refactor: DurabilityFormatter util uses `String.format`

--- src/main/java/com/caedis/duradisplay/utils/DurabilityFormatter.java
+++ src/main/java/com/caedis/duradisplay/utils/DurabilityFormatter.java
@@ -2,11 +2,7 @@ package com.caedis.duradisplay.utils;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.text.NumberFormat;
-
-
-public class DurabilityLikeFormatter {
-    private static final NumberFormat nf = NumberFormat.getNumberInstance();
+public class DurabilityFormatter {
 
     public enum Format {
         percent,
@@ -16,25 +12,24 @@ public class DurabilityLikeFormatter {
         fraction,
     }
 
-
     @Nullable
-    public static String format(double current,double max,Format format) {
+    public static String format(double current, double max, Format format) {
         double percent = current / max * 100;
         switch (format) {
             case percent -> {
-                return Double.isNaN(percent) ? null : nf.format(percent) + "%";
+                return Double.isNaN(percent) ? null : String.format("%.0f%%", percent);
             }
             case remain -> {
-                return nf.format(current);
+                return String.format("%.0f", max - current);
             }
             case used -> {
-                return nf.format(max - current);
+                return String.format("%.0f", current);
             }
             case max -> {
-                return nf.format(max);
+                return String.format("%.0f", max);
             }
             case fraction -> {
-                return nf.format(current) + "/" + nf.format(max);
+                return Double.isNaN(percent) ? null : String.format("%.0f/%.0f", current, max);
             }
         }
         return null;

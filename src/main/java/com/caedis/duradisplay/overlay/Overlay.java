9dc1c1e9c0e3cf2a2fa6755f4024e022bf30fbbc
ghostflyby <ghostflyby+git@outlook.com>
2023-09-06 18:41:50 +0800

    Overlay abstract class method change and comments

--- src/main/java/com/caedis/duradisplay/overlay/Overlay.java
+++ src/main/java/com/caedis/duradisplay/overlay/Overlay.java
@@ -8,6 +8,15 @@ import org.jetbrains.annotations.Nullable;
 import com.caedis.duradisplay.config.Config;
 import com.caedis.duradisplay.render.OverlayRenderer;
 
+/**
+ * <p>
+ * The base type of all Overlays
+ * <p/>
+ * By extending this, non-abstract classes will be automatically registered as an Overlay
+ * with config automatically loaded at launch and registered in the GUI
+ * 
+ * @param <C> Config type extending {@link Config}
+ */
 @com.caedis.duradisplay.annotation.Overlay
 public abstract class Overlay<C extends Config> {
 
@@ -15,7 +24,5 @@ public abstract class Overlay<C extends Config> {
     abstract C config();
 
     @Nullable
-    public OverlayRenderer getRenderer(@NotNull ItemStack itemStack) {
-        return null;
-    }
+    public abstract OverlayRenderer getRenderer(@NotNull ItemStack itemStack);
 }

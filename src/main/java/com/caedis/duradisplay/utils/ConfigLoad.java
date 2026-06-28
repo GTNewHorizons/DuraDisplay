1dc24a4077d319a093bf7a374468ea2effbd2d72
Federico <masifede2@gmail.com>
2026-06-28 11:51:13 +0200

    Update ConfigLoad.java

--- src/main/java/com/caedis/duradisplay/utils/ConfigLoad.java
+++ src/main/java/com/caedis/duradisplay/utils/ConfigLoad.java
@@ -12,17 +12,25 @@ public class ConfigLoad {
         String comment) {
 
         Class<T> enumType = defaultValue.getDeclaringClass();
-        return T.valueOf(
-            enumType,
-            config.getString(
-                name,
-                category,
-                defaultValue.toString(),
-                comment,
-                Arrays.stream(enumType.getEnumConstants())
-                    .map(Enum::toString)
-                    .toArray(String[]::new)));
+        String value = config.getString(
+            name,
+            category,
+            defaultValue.toString(),
+            comment,
+            Arrays.stream(enumType.getEnumConstants())
+                .map(Enum::toString)
+                .toArray(String[]::new));
 
+        // Match case-insensitively and fall back to the default instead of throwing.
+        for (T constant : enumType.getEnumConstants()) {
+            if (constant.name()
+                .equalsIgnoreCase(value)
+                || constant.toString()
+                    .equalsIgnoreCase(value)) {
+                return constant;
+            }
+        }
+        return defaultValue;
     }
 
     public static <T extends Enum<T>> T loadEnum(String category, String name, T defaultValue, String comment) {

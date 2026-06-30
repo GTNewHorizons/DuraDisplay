0bbfdaeaeaba225f292d4952b1ddbdd77c5e16c3
Caedis <Caedis@users.noreply.github.com>
2026-06-30 14:16:50 -0500

    feat(config): parse enum values case-insensitively (#28)

--- src/main/java/com/caedis/duradisplay/utils/ConfigLoad.java
+++ src/main/java/com/caedis/duradisplay/utils/ConfigLoad.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import net.minecraftforge.common.config.Configuration;
 
+import com.caedis.duradisplay.DuraDisplay;
 import com.caedis.duradisplay.config.DuraDisplayConfig;
 
 public class ConfigLoad {
@@ -12,17 +13,30 @@ public class ConfigLoad {
         String comment) {
 
         Class<T> enumType = defaultValue.getDeclaringClass();
-        return T.valueOf(
-            enumType,
-            config.getString(
-                name,
-                category,
-                defaultValue.toString(),
-                comment,
-                Arrays.stream(enumType.getEnumConstants())
-                    .map(Enum::toString)
-                    .toArray(String[]::new)));
+        String raw = config.getString(
+            name,
+            category,
+            defaultValue.toString(),
+            comment,
+            Arrays.stream(enumType.getEnumConstants())
+                .map(Enum::toString)
+                .toArray(String[]::new));
 
+        for (T constant : enumType.getEnumConstants()) {
+            if (constant.name()
+                .equalsIgnoreCase(raw)) {
+                return constant;
+            }
+        }
+
+        DuraDisplay.LOG.warn(
+            String.format(
+                "Invalid value '%s' for %s.%s, falling back to default '%s'",
+                raw,
+                category,
+                name,
+                defaultValue.name()));
+        return defaultValue;
     }
 
     public static <T extends Enum<T>> T loadEnum(String category, String name, T defaultValue, String comment) {
